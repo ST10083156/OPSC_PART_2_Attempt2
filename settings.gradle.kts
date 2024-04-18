d7852bed151ad49136140040e3d19b2905c4bd5e
st10083156 <st10083156@vcconnect.edu.za>
2024-04-18 10:46:07 +0200

    Login and registration, profile details (partially) added

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "OPSC_PART_2_Attempt2"
+include(":app")
+ 
