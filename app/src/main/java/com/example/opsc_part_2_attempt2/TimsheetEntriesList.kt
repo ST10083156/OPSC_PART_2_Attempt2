89e355829a45eaa611eb6efca33036138ff35ca5
st10083156 <st10083156@vcconnect.edu.za>
2024-04-23 11:12:30 +0200

    comments added

--- app/src/main/java/com/example/opsc_part_2_attempt2/TimsheetEntriesList.kt
+++ app/src/main/java/com/example/opsc_part_2_attempt2/TimsheetEntriesList.kt
@@ -5,16 +5,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.opsc_part_2_attempt2.databinding.ActivityTimsheetEntriesListBinding
 
+//timesheet list that shows all the entries by the user in the firestore db
 class TimsheetEntriesList : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_timsheet_entries_list)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        val binding = ActivityTimsheetEntriesListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //this part just makes sure that the content on the screen is offset...
+        //...past any system displays like the task bar or anything else
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { view, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+
         }
+
     }
-}
+
+
+    }
